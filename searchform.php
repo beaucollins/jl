d404037999e26009e1386c1882ae43110a6e9e39
Beau Collins <beaucollins@gmail.com>
2010-04-08 14:28:24 -0700

    resetting starting with only what we need

--- searchform.php
+++
@@ -1,7 +0,0 @@
-    <form id="searchform" name="searchform" method="get" action="<?php echo home_url(); ?>">
-		<div>
-			<label for="s"><?php _e( 'Search', 'twentyten' ); ?></label>
-			<input type="text" id="s" name="s" />
-			<input type="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'twentyten' ); ?>" />
-		</div>
-    </form>
