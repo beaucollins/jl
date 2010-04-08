f677d939d6f9433536febfb6a1a7096178296172
Beau Collins <beaucollins@gmail.com>
2010-04-08 09:09:02 -0700

    starting jl's theme based on wp twentyten theme

---
+++ category.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+		<div id="container">
+			<div id="content">
+
+				<h1 class="page-title"><?php
+					printf( __( 'Category Archives: %s', 'twentyten' ), '<span>' . single_cat_title( '', false ) . '</span>' );
+				?></h1>
+				<?php $categorydesc = category_description(); if ( ! empty( $categorydesc ) ) echo apply_filters( 'archive_meta', '<div class="archive-meta">' . $categorydesc . '</div>' ); ?>
+
+				<?php
+				/* Run the loop for the category page to output the posts.
+				 * If you want to overload this in a child theme then include a file
+				 * called loop-category.php and that will be used instead.
+				 */
+				 get_template_part( 'loop', 'category' );
+				?>
+
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
---
+++ footer.php
@@ -0,0 +1,24 @@
+	</div><!-- #main -->
+
+	<div id="footer">
+		<div id="colophon">
+
+<?php get_sidebar( 'footer' ); ?>
+
+			<div id="site-info">
+				<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+			</div>
+
+			<div id="site-generator">
+				<?php printf( __( 'Proudly powered by <span id="generator-link">%s</span>.', 'twentyten' ), '<a href="http://wordpress.org/" title="' . esc_attr__( 'Semantic Personal Publishing Platform', 'twentyten' ) . '" rel="generator">' . __( 'WordPress', 'twentyten' ) . '</a>' ); ?>
+			</div>
+
+		</div><!-- #colophon -->
+	</div><!-- #footer -->
+
+</div><!-- #wrapper -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
